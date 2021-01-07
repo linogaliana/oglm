f1f0439fcc5e51c094f2be8b81eec36fc0e6cbb1
Lino Galiana <linogaliana@yahoo.fr>
2021-01-07 10:28:30 +0100

    move print.margins

--- R/print_margins.R
+++ R/print_margins.R
@@ -1,19 +1,3 @@
-# collection of functions used to produce regression output
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 print.margins.oglmx<-function(x, ... ){
   for (m in 1:length(x)){
     cat("Marginal Effects on Pr(Outcome==",names(x)[m],")","\n",sep="")
