26b7071415f81b7fa26536654db2158adc776ec4
Lino Galiana <linogaliana@yahoo.fr>
2021-01-07 10:23:37 +0100

    extraction des fonctions logLik

--- R/genOutput.R
+++ R/genOutput.R
@@ -31,23 +31,6 @@ nobs.oglmx<-function(object, ...){
   return(attr(object$loglikelihood,"No.Obs"))
 }
 
-# working
-logLik.oglmx<-function(object, ...){
-  value<-object$loglikelihood[1]
-  attr(value,"df")<-length(object$coefficients)
-  return(value)
-}
-
-# works with output of new version, need to check with output of old version
-.BaseLL<-function(object){
-  data<-object$NoVarModData
-  if (ncol(data)==2){
-    BaseLL<-as.numeric(logLik(oglmx(Y~1,data=data,weights=data$weights)))
-  } else {
-    BaseLL<-as.numeric(logLik(oglmx(Y~1,data=data)))
-  }
-  return(BaseLL)
-}
 
 
 # working
@@ -115,13 +98,6 @@ McFaddensR2.oglmx<-function(object){
   return(value)
 }
 
-
-# works
-logLik.summary.oglmx<-function(object, ...){
-  value<-object$loglikelihood[1]
-  attr(value,"df")<-length(object$coefficients)
-  return(value)
-}
 
 
 # works
