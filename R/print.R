e124b1ea3db6648fcfbb9cf836578c22e9fdf15f
Lino Galiana <linogaliana@yahoo.fr>
2021-01-12 09:58:47 +0100

    print more information

--- R/print.R
+++ R/print.R
@@ -6,6 +6,8 @@ print.oglmx <- function(x, digits = max(3L, getOption("digits") - 3L),
   cat("\nCall:  ", paste(deparse(x$call), sep = "\n", collapse = "\n"),
       "\n\n", sep = "")
 
+  std <- diag(x$vcov)^0.5
+
   if (length(coef(x))){
 
     cat("Coefficients")
@@ -13,9 +15,22 @@ print.oglmx <- function(x, digits = max(3L, getOption("digits") - 3L),
     if (is.character(co <- x$contrasts))
       cat("  [contrasts: ", apply(cbind(names(co), co),
                                   1L, paste, collapse = "="), "]")
+
+    formated_coef <- paste0(
+      format(x$coefficients, digits = digits),
+      signif_stars_vectorized(2*pnorm( -abs(x$coefficients/std)),
+                              type = "none"),
+      sprintf(" (%s)",format(std, digits = digits))
+    )
+    names(formated_coef) <- names(x$coefficients)
+
     cat(":\n")
-    print.default(format(x$coefficients, digits = digits),
-                  print.gap = 2, quote = FALSE)
+    print.default(
+      formated_coef,
+      print.gap = 2,
+      quote = FALSE
+    )
+
   } else{
     cat("No coefficients\n\n")
   }
@@ -24,7 +39,10 @@ print.oglmx <- function(x, digits = max(3L, getOption("digits") - 3L),
 
   if (nzchar(mess <- naprint(x$na.action)))
     cat("  (", mess, ")\n", sep = "")
-  cat("Log likelihood by observation:\t   ",
+
+  cat("Log likelihood:\t",
+      format(signif(x$loglikelihood, digits)),
+      "\nLog likelihood by observation:\t   ",
       format(signif(x$loglikelihood/attr(x$loglikelihood, "No.Obs"),
                     digits)),
       "\nAIC:\t", format(signif(AIC(x), digits)))
