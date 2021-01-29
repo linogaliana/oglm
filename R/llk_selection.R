b69c116f707504f54998d54e26a067cc15fdea1a
Lino Galiana <linogaliana@yahoo.fr>
2021-01-29 19:07:34 +0100

    premiers éléments pour sélection

---
+++ R/llk_selection.R
@@ -0,0 +1,42 @@
+llk_selection <- function(y_selection, beta, X, gamma, Z,
+                          y_outcome, thresholds, rho, sigma){
+
+  rho_matrix <- matrix(c( 1, -rho, -rho, 1), nrow = 2 )
+
+  llk_censor <- (1 - y_selection)*log(pnorm(-gamma %*% Z))
+  llk_observed <- y_selection*log(
+    mvtnorm::pmvnorm((thresholds[m + 1] - beta %*% X)/sigma,
+                     (thresholds[m] - beta %*% X)/sigma,
+                     sigma = rho_matrix)
+  )
+
+  llk <- llk_censor + llk_observed
+
+  return(llk)
+}
+
+
+dens_mvnorm <- function(x1,x2,rho){
+
+  rho_matrix <- matrix(c( 1, rho, rho, 1), nrow = 2 )
+  dens <- mvtnorm::dmvnorm(c(x1, x2), sigma = rho_matrix)
+
+  return(dens)
+}
+
+dPhidx2 <- function(x1,x2,rho){
+
+  return(
+    pnorm((x1 - rho*x2)/sqrt(1-rho^2))*dnorm(x2)
+  )
+}
+
+dPhidx1 <- function(x1,x2,rho){
+
+  return(
+    pnorm((x2 - rho*x1)/sqrt(1-rho^2))*dnorm(x1)
+  )
+}
+
+
+
---
+++ tests/testthat/test-gradient_llk_selection.R
@@ -0,0 +1,37 @@
+
+
+x1 <- rnorm(1)
+x2 <- rnorm(1)
+rho <- runif(1)
+
+testthat:::test_that("Numerical derivatives for multivariate normal ok",{
+  testthat::expect_equal(
+    dens_mvnorm(x1,x2,rho),
+    exp(
+      - (x1^2 - 2*rho*x1*x2 + x2^2)/(2*(1-rho^2))
+    )/(2*pi*sqrt(1 - rho^2))
+  )
+
+  grad_x1 <- maxLik::numericGradient(
+    function(x){mvtnorm::pmvnorm(upper = c(x,x2) ,
+                                  sigma = matrix(c(1, rho, rho, 1),
+                                                 nrow = 2))},
+    x1
+  )
+  grad_x2 <- maxLik::numericGradient(
+    function(y){mvtnorm::pmvnorm(upper = c(x1,y) ,
+                                   sigma = matrix(c(1, rho, rho, 1),
+                                                  nrow = 2))},
+    x2
+  )
+  testthat::expect_equal(
+    as.numeric(grad_x1),
+    dPhidx1(x1,x2,rho)
+  )
+  testthat::expect_equal(
+    as.numeric(grad_x2),
+    dPhidx2(x1,x2,rho)
+  )
+})
+
+
