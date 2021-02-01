e6529725198df9ca60043609ffa436c848c47483
Lino Galiana <linogaliana@yahoo.fr>
2021-02-01 12:00:40 +0100

    prepare gradient
    :warning: pas encore fonctionnel

--- R/llk_selection.R
+++ R/llk_selection.R
@@ -47,7 +47,7 @@ dPhidrho <- function(x1,x2,rho){
 
 
 dPhidgamma <- function(x, z, rho, sigma,
-                                beta, gamma, alpha_m){
+                       beta, gamma, alpha_m){
 
   dphi_eval <- dPhidx2((alpha_m - x %*% beta) /sigma, z %*% gamma, -rho)
 
@@ -59,7 +59,7 @@ dPhidgamma <- function(x, z, rho, sigma,
 
 
 dPhidbeta <- function(x, z, rho, sigma,
-                       beta, gamma, alpha_m){
+                      beta, gamma, alpha_m){
 
 
   dphi_eval <- dPhidx1((alpha_m - x %*% beta) /sigma, z %*% gamma, -rho)
@@ -69,3 +69,40 @@ dPhidbeta <- function(x, z, rho, sigma,
   )
 
 }
+
+
+dllkdbeta <- function(y_selection, beta, X, gamma, Z,
+                      y_outcome, thresholds, rho, sigma){
+
+  rho_matrix <- matrix(c( 1, -rho, -rho, 1), nrow = 2 )
+
+  p1 <- -(1 - y_selection)*inverse_mills_ratio(- z %*% gamma) %*% z
+
+  p2 <- wrap_pnorm((thresholds[m + 1] - X %*% beta)/sigma,
+                   z %*% gamma,
+                   rho)
+  p2 <- p2 - wrap_pnorm((thresholds[m] - X %*% beta)/sigma,
+                        z %*% gamma,
+                        rho)
+  p2 <- p2*dnorm(z %*% gamma)*z
+  p2_denom <- mvtnorm::pmvnorm((thresholds[m + 1] - beta %*% X)/sigma,
+                               (thresholds[m] - beta %*% X)/sigma,
+                               sigma = rho_matrix)
+
+  p2 <- p2/p2_denom
+
+
+  return(p2)
+}
+
+inverse_mills_ratio <- function(x){
+  dnorm(x)/pnorm(x)
+}
+
+
+wrap_pnorm <- function(x1,x2,rho){
+  return(
+    pnorm((x1 + rho*x2)/sqrt(1 - rho^2))
+  )
+}
+
